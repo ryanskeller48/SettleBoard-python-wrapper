b722aabb6f1047e3d963ebdf87bb11c0ae1503a2
Ryan Keller <ryan.keller@hanzoarchives.com>
2020-04-06 18:54:10 +0000

    more input verification and verbose error output

--- run.py
+++ run.py
@@ -37,25 +37,98 @@ def main():
     # execute desired user action
 
     if options.create_match:
+
+        # first see if the user has at least provided 6 input arguments (3 scores, 3 names)
         try:
             p1 = str(sys.argv[2])
-            s1 = int(sys.argv[3])
+            s1 = sys.argv[3]
             p2 = str(sys.argv[4])
-            s2 = int(sys.argv[5])
+            s2 = sys.argv[5]
             p3 = str(sys.argv[6])
-            s3 = int(sys.argv[7])
-            
-            # TODO: catch bad input types (e.g. score>11 or score<2) and return verbose error message
-            try: # 4 man game
+            s3 = sys.argv[7]
+
+            # make sure scores are integers and > 2 and < 11
+            tick = 1
+            winners = 0
+            scores = [s1, s2, s3]
+            for score in scores:
+                try:
+                    score = int(score)
+                    if score < 2:
+                        print(f"Score {tick} must be >= 2! You entered: {score}")
+                        sys.exit(1)
+                    elif score > 11:
+                        print(f"Score {tick} must be <= 11! You entered: {score}")
+                        sys.exit(1)
+                    elif score >= 10:
+                        winners += 1
+                except ValueError:
+                    print(f"Score {tick} must be an integer! You entered: {score}")
+                    sys.exit(1)
+                tick += 1
+
+            try: # 4 man game if this succeeds
                 p4 = str(sys.argv[8])
-                s4 = int(sys.argv[9])
-                response = sb.create_match(p1, s1, p2, s2, p3, s3, p4=p4, s4=s4)
-                print (response)
-            except: # 3 man game
-                response = sb.create_match(p1, s1, p2, s2, p3, s3)
-                print (response)
-        except:
-            print ("Not enough arguments! Need 6 or 8")
+                s4 = sys.argv[9]
+                
+                # make sure score 4 is an integer and > 2 and < 11
+                try:
+                    score = int(score)
+                    if score < 2:
+                        print(f"Score 4 must be >= 2! You entered: {score}")
+                        sys.exit(1)
+                    elif score > 11:
+                        print(f"Score 4 must be <= 11! You entered: {score}")
+                        sys.exit(1)
+                    elif score >= 10:
+                        winners += 1
+                    
+                    # verify there is exactly one winning (>10) score
+                    if winners < 1:
+                        print ("No winning score!")
+                        sys.exit(1)
+                    elif winners > 1:
+                        print ("Too many winning scores!")
+                        sys.exit(1)
+
+                except ValueError:
+                    print(f"Score 4 must be an integer! You entered: {score}")
+                    sys.exit(1)
+
+                # try to make game
+                response = sb.create_match(p1, int(s1), p2, int(s2), p3, int(s3), p4=p4, s4=int(s4))
+
+                if response is not None: # 200 good query
+                    print (response)
+                else:
+                    # one of the names was bad, scores already checked
+                    for name in [p1, p2, p3, p4]:
+                        if not sb.has_user(name):
+                            print (f"User {name} does not exist!")
+
+            except IndexError: # 3 man game since there were less than 8 arguments
+
+                # verify there is exactly one winning (>10) score
+                if winners < 1:
+                    print ("No winning score!")
+                    sys.exit(1)
+                elif winners > 1:
+                    print ("Too many winning scores!")
+                    sys.exit(1)
+
+                # try to make game
+                response = sb.create_match(p1, int(s1), p2, int(s2), p3, int(s3))
+
+                if response is not None: # 200 good query
+                    print (response)
+                else:
+                    # one of the names was bad, scores already checked
+                    for name in [p1, p2, p3]:
+                        if not sb.has_user(name):
+                            print (f"User {name} does not exist!")
+
+        except IndexError: # incorrect number of input arguments
+            print ("Bad number of arguments! Need 6 or 8. See --help")
 
     elif options.get_users == "True":
         response = sb.get_users()
@@ -66,8 +139,7 @@ def main():
             name = str(sys.argv[2])
             response = sb.get_user_id(name)
             print (response)
-        except:
-            # TODO: add better input checking for this and below similar conditionals
+        except IndexError: # no name provided
             print ("Provide a name!")
 
     elif options.has_user:
@@ -75,7 +147,7 @@ def main():
             name = str(sys.argv[2])
             response = sb.has_user(name)
             print (response)
-        except:
+        except IndexError: # no name provided
             print ("Provide a name!")
 
     elif options.user_last_match:
@@ -83,7 +155,7 @@ def main():
             name = str(sys.argv[2])
             response = sb.get_last_user_match(name)
             print (response)
-        except:
+        except IndexError: # no name provided
             print ("Provide a name!")
 
     elif options.get_leaderboard == "True":
@@ -96,8 +168,7 @@ def main():
             password = str(sys.argv[3])
             response = sb.make_user(name, password)
             print (response)
-        except:
-            # TODO: add better input checking
+        except IndexError: # have 1 or 0 input arguments, need 2
             print ("Provide a name and password!")
 
     else: # no option provided
